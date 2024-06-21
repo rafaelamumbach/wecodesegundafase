03c048cc8b6a550f1d58d6fe62b52e1a4a669eb0
Rafaela <seuemail@example.com>
2024-06-21 15:22:42 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import './App.scss';
-import logo from './logo.svg';
+import React from "react";
+import "./App.scss";
+import logo from "./imgs/Logo.png";
+import produtos from "./imgs/Produtos.png";
+import lancamentos from "./imgs/Lancamentos.png";
+import outlet from "./imgs/Outlet.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1 className="hd1">Você está em:</h1>
+        <h1 className="localizacao">São Paulo</h1>
+        <h1 className="alterar">Alterar</h1>
       </header>
+      <div className="opcoes">
+        <img src={logo} alt="Logo Bebece" className="logo" />
+        <img src={produtos} alt="Produtos" className="produtos" />
+        <img src={lancamentos} alt="Lancamentos" className="lancamentos" />
+        <img src={outlet} alt="Outlet" className="outlet" />
+      </div>
     </div>
   );
 }
 
 export default App;
+
